8999f92dda15cae4b1ec64453df5bbaa2f383d53
Vinicius Lemos <vinycius1993@gmail.com>
2023-01-14 20:34:02 -0300

    Implementado a função de Editar dados

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,22 +2,39 @@ class Produto {
     constructor() {
         this.id = 1
         this.arrayProdutos = []
+        this.editId = null;//Quando este ítem estiver com valor diferente de null
+                           //quer dizer que vc está fazendo uma edição e se estiver null vc está inserindo novo dado.
     }
 
     salvar() {
         let produto = this.lerDados()
         //Validando se a função validarCampos retornou verdadeiro (se retornou verdadeiro é pq os campos foram preenchidos)
         if (this.validarCampos(produto) == true) {
-            this.adicionar(produto)
+            if (this.editId == null) {
+                this.adicionar(produto)
+            }else {
+                this.atualizar(this.editId, produto)
+            }
+            
 
         }
         this.listaTabela()
         this.cancelar()
     }
     adicionar(produto) {
+        produto.preco = parseFloat(produto.preco)
         //a função push irá adicionar os campos dentro do array
         this.arrayProdutos.push(produto)
         this.id++
+    }
+
+    atualizar(id, produto) {
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            if (this.arrayProdutos[i].id == id) {
+                this.arrayProdutos[i].nomeProduto = produto.nomeProduto
+                this.arrayProdutos[i].preco = produto.preco
+            }
+        }
     }
 
     listaTabela() {
@@ -39,17 +56,17 @@ class Produto {
 
             let imgEdit = document.createElement('img')
             imgEdit.src = 'assets/img/edit.png';
+            imgEdit.setAttribute("onclick", "produto.preparaEdicao("+ JSON.stringify(this.arrayProdutos[i]) +")")
 
             let imgDelete = document.createElement('img')
             imgDelete.src = 'assets/img/delete.png';
             /*O setAttribute é uma função que recebe dois parâmetros, o primeiro é o evento que
             vc quer disparar, e o segundo é a função que será chamada.
-            Neste caso colocamos o evento de click nas imagens de alterar e deletar, chamando
+            Neste caso colocamos o evento de click na imagem de deletar, chamando
             suas respectivas funções
             Obs, ao chamar a função, precisamos saber qual linha vamos deletar ou alterar, para isso
             vamosconcatenar com o arrayProdutos na posição [i] que já temos.
             */
-
             imgDelete.setAttribute("onclick", "produto.deletar("+ this.arrayProdutos[i].id +")")
 
             td_acoes.appendChild(imgEdit);
@@ -87,6 +104,8 @@ class Produto {
         document.getElementById('produto').value = ''
         document.getElementById('preco').value = ''
 
+        document.getElementById('btn1').innerText = 'Salvar'
+        this.editId = null
     }
     deletar(id) {
         if (confirm (`Deseja realmente Deletar o produto do ID ${id}`)) {
@@ -104,6 +123,13 @@ class Produto {
             }
         }
         }
+    }
+    preparaEdicao(dados) {
+        this.editId = dados.id
+        document.getElementById('produto').value = dados.nomeProduto
+        document.getElementById('preco').value = dados.preco
+        document.getElementById('btn1').innerText = 'Atualizar'
+
     }
 }
 //Criando um objeto do tipo Produto e atribuindo a variável produto.
