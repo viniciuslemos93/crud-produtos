6c626e0675661e30e182f8d91dea433b9d7d80c1
Vinicius Lemos <vinycius1993@gmail.com>
2023-01-14 18:54:28 -0300

    Implementada a função de inserir produtos

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,16 +1,76 @@
 class Produto {
     constructor() {
-        this.id = 0;
-        this.nomeProduto = '';
-        this.valor = 0;
+        this.id = 1
+        this.arrayProdutos = []
     }
 
-    adicionar() {
-        alert("Adicionar vou acionado!")
+    salvar() {
+        let produto = this.lerDados()
+        //Validando se a função validarCampos retornou verdadeiro (se retornou verdadeiro é pq os campos foram preenchidos)
+        if (this.validarCampos(produto) == true) {
+            this.adicionar(produto)
+
+        }
+        this.listaTabela()
+    }
+    adicionar(produto) {
+        //a função push irá adicionar os campos dentro do array
+        this.arrayProdutos.push(produto)
+        this.id++
     }
 
-    excluir() {
-        alert("ítem deletado!")
+    listaTabela() {
+        let tbody = document.getElementById('tbody')
+        tbody.innerText = '' //Para não duplicar os dados na tabela.
+        for (let i = 0; i < this.arrayProdutos.length; i++) {
+            let tr = tbody.insertRow()
+
+            let td_id = tr.insertCell()
+            let td_produto = tr.insertCell()
+            let td_preco = tr.insertCell()
+            let acoes = tr.insertCell()
+
+            td_id.innerText = this.arrayProdutos[i].id
+            td_produto.innerText = this.arrayProdutos[i].nomeProduto
+            td_preco.innerText = this.arrayProdutos[i].preco
+
+            td_id.classList.toggle('center');
+
+            let imgEdit = document.createElement('img')
+            imgEdit.src = '../img/edit.png'
+
+            td_acoes.appendChild(imgEdit);
+        }
+    }
+
+    lerDados() {
+        let produto = {}
+            
+            produto.id = this.id
+            produto.nomeProduto = document.getElementById('produto').value
+            produto.preco = document.getElementById('preco').value
+
+            return produto        
+    }
+
+    validarCampos(produto) {
+        let msg = ''
+        
+        if (produto.nomeProduto == '') {
+            msg += 'Informe o nome do Produto \n'
+        }
+        if (produto.preco == '') {
+            msg += 'Informe o preço do Produto \n'
+        }
+        if (msg != '') {
+            alert(msg)
+            return false
+        }
+        return true
+    }
+
+    cancelar() {
+        
     }
 }
 //Criando um objeto do tipo Produto e atribuindo a variável produto.
